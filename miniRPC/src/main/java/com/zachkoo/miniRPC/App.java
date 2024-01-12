bec33ec149203f5d38b564ab7597ecefff4356ff
zachkoo <zacharykoo@g.ucla.edu>
2024-01-11 21:46:54 -0800

    test commit

---
+++ miniRPC/src/main/java/com/zachkoo/miniRPC/App.java
@@ -0,0 +1,13 @@
+package com.zachkoo.miniRPC;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Worlddd!" );
+    }
+}
